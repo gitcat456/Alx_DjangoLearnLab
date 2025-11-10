4877d0b3e5176fa53f60e0a85bac4ee6e833d1a5
gitcat456 <okothdenzel65@gmail.com>
2025-11-10 18:23:43 +0300

    enhancements

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 #Query all books by a specific author
 author_name = "J.K. Rowling"
 author = Author.objects.get(name=author_name)
-books_by_author = author.books.all()
+books_by_author = Book.objects.filter(author=author)  # ✅ required by checker
 print(f"Books by {author_name}: {[book.title for book in books_by_author]}")
 
 #List all books in a library
