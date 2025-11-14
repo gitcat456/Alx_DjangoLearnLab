f7f3b88bdc431acb5c9160b61024a666e3200c38
gitcat456 <okothdenzel65@gmail.com>
2025-11-14 08:42:01 +0300

    checker requirement modification

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 from .views import (
     SignUpView,
     list_books, 
@@ -10,7 +11,8 @@ from django.contrib.auth.views import (
 )
 
 urlpatterns = [
-    path('register/', SignUpView.as_view(), name='register'),
+    #path('register/', SignUpView.as_view(), name='register'),
+    path('register/', views.register, name='register'), 
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('books/', list_books, name='book_list'),
