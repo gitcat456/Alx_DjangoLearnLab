9c81ed6a0c28c473146b3d8ee328ef3260ffafdf
gitcat456 <okothdenzel65@gmail.com>
2025-11-14 08:24:31 +0300

    creates a SignUpView Class for registration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from  relationship_app.models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
 
 def list_books(request):
     all_books = Book.objects.all()
