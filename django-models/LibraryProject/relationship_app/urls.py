9797df139107eeb533e0f70d79b4c295d7087d3e
gitcat456 <okothdenzel65@gmail.com>
2025-11-14 09:09:05 +0300

    secure book management urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,7 @@
 from django.urls import path
 from . import views
-from .views import (
-    SignUpView,
-    list_books, 
-    LibraryDetailView, 
-    admin_view,
-    librarian_view,
-    member_view
-    )
+from .views import (list_books, LibraryDetailView, admin_view, 
+                   librarian_view, member_view, add_book, edit_book, delete_book)
 from django.contrib.auth.views import (
     LoginView,
     LogoutView,
@@ -25,4 +19,9 @@ urlpatterns = [
     path('admin/dashboard/', admin_view, name='admin_view'),
     path('librarian/dashboard/', librarian_view, name='librarian_view'),
     path('member/dashboard/', member_view, name='member_view'),
+    
+     # SECURED BOOK MANAGEMENT URLS
+    path('books/add/', add_book, name='add_book'),
+    path('books/<int:book_id>/edit/', edit_book, name='edit_book'),
+    path('books/<int:book_id>/delete/', delete_book, name='delete_book'),
 ]
