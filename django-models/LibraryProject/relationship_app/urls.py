9e6e7ff99677b386e8101e157b9ec6fd538498ea
gitcat456 <okothdenzel65@gmail.com>
2025-11-14 08:25:33 +0300

    paths and views for basic authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import (
+    SignUpView,
+    list_books, 
+    LibraryDetailView, 
+    )
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+)
 
 urlpatterns = [
+    path('register/', SignUpView.as_view(), name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('books/', list_books, name='book_list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail')
 ]
