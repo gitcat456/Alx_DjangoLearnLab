931af01f63c728458c2b57c946a202b60091fd8a
gitcat456 <okothdenzel65@gmail.com>
2025-11-14 07:36:29 +0300

    refactors

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,13 +3,13 @@ from  relationship_app.models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
 
-def book_list(request):
+def list_books(request):
     all_books = Book.objects.all()
     context = {'books': all_books}
     return render(request, 'relationship_app/list_books.html', context)
     
 
-class LibBooks(DetailView):
+class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
