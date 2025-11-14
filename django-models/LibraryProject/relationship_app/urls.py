c8aa551bdc50c2629db2206587e2e95d08e9f7b7
gitcat456 <okothdenzel65@gmail.com>
2025-11-14 07:36:47 +0300

    refactors

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from . import views
-from .views import LibBooks, book_list
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('books/', views.book_list, name='book_list'),
-    path('library/<int:pk>/', LibBooks.as_view(), name='library_detail')
+    path('books/', list_books, name='book_list'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail')
 ]
