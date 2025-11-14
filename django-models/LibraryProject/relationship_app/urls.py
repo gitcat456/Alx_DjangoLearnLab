f94c56fe18b8a392aff9a6c8a36463843c9d7351
gitcat456 <okothdenzel65@gmail.com>
2025-11-14 08:57:00 +0300

    adds role based URLs

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,6 +4,9 @@ from .views import (
     SignUpView,
     list_books, 
     LibraryDetailView, 
+    admin_view,
+    librarian_view,
+    member_view
     )
 from django.contrib.auth.views import (
     LoginView,
@@ -16,5 +19,10 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('books/', list_books, name='book_list'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail')
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    
+     # Role-based URLs
+    path('admin/dashboard/', admin_view, name='admin_view'),
+    path('librarian/dashboard/', librarian_view, name='librarian_view'),
+    path('member/dashboard/', member_view, name='member_view'),
 ]
