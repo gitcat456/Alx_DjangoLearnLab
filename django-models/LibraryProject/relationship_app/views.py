a7e7f4d4cd408079d64b5950616143280aed3f2a
gitcat456 <okothdenzel65@gmail.com>
2025-11-14 09:12:19 +0300

    checker requirement refactor

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,7 +7,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from .models import UserProfile
 from django.contrib import messages
 
