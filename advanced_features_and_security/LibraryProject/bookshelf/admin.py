72f43826bde15c3e71f4446c11dad69bd6fa16f3
gitcat456 <okothdenzel65@gmail.com>
2025-11-16 15:56:56 +0300

    checker requirements

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,29 @@
+    
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')  # shows these columns
     list_filter = ('publication_year', 'author')  # adds filter sidebar
-    search_fields = ('title', 'author')  # adds a search bar
+
+# ----------------------------
+# Custom User Admin
+# ----------------------------
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    # Add the extra fields to both edit and create forms in admin
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+# Register CustomUser with custom admin
+admin.site.register(CustomUser, CustomUserAdmin)
+
+
+
+
