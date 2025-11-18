af77d1d7d4a08f9f483e2247b44e7b8c86014c3e
gitcat456 <okothdenzel65@gmail.com>
2025-11-18 12:12:24 +0300

    User= get_user_model

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+User = get_user_model()
 
 class Author(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.name  # shows the author's name in admin and shell
+        return self.name  
 
 
 class Book(models.Model):
