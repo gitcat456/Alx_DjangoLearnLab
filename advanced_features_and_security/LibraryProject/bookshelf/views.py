df27394329f2723cfe07d009e91fb590bb42aa50
gitcat456 <okothdenzel65@gmail.com>
2025-11-18 06:30:36 +0300

    setsup permissions

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(
+        request,
+        'bookshelf/book_list.html',
+        {'books': books}
+    )
+    
+    
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+     return render(request, 'bookshelf/create_book.html')
+
+permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, pk):
+    # editing logic...
+    return render(request, 'bookshelf/edit_book.html')
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, pk):
+    # delete logic...
+    return render(request, 'bookshelf/book_list.html')
