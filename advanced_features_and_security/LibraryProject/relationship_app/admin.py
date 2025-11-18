0059b4115e0e614ebfb01cecc37b1bde066cae2d
gitcat456 <okothdenzel65@gmail.com>
2025-11-18 14:25:04 +0300

    registers the Author model to the site

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Author
 
-# Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    
+admin.site.register(Author, AuthorAdmin)
