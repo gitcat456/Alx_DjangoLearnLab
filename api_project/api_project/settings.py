85ccd56cb58491c9d2edc5c462fee51cd0c14d58
gitcat456 <okothdenzel65@gmail.com>
2025-11-22 18:37:48 +0300

    token authentication configuration

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -124,3 +124,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # Optional, for browsable API
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # Optional: require auth by default
+    ],
+}
