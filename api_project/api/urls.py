34d9e4b78ff34ab142821fee45d3df191d2aa75a
gitcat456 <okothdenzel65@gmail.com>
2025-11-22 18:26:12 +0300

    minor fix

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -7,5 +7,5 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
-    path('', include(router.get_urls))
+    path('', include(router.urls))
 ]
