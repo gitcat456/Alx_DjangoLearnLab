b882ab5febe069bfedabbd73787a79a15c27dc45
gitcat456 <okothdenzel65@gmail.com>
2025-11-22 18:52:56 +0300

    custom permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .serializers import BookSerializer
 from .models import Book
-from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny, IsAuthenticatedOrReadOnly
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [AllowAny]
 
 class BookViewSet(viewsets.ModelViewSet):
      queryset = Book.objects.all()
      serializer_class = BookSerializer
+    # permission_classes = [IsAuthenticatedOrReadOnly] 
+    
+     def get_permissions(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
