4c52c02a3c8ab240748f3e32151fe3481b973c43
gitcat456 <okothdenzel65@gmail.com>
2025-11-22 18:02:50 +0300

    creates a boook model

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=50)
+    published_date = models.DateField()
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
