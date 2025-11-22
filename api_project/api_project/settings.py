a08a32b7900282eca1879def3e99d7434b6f5cec
gitcat456 <okothdenzel65@gmail.com>
2025-11-22 18:36:56 +0300

    adds the authtoken app

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
