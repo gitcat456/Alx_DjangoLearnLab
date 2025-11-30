7ec448322e3c51a86e5a66028ab161582b33f2cb
gitcat456 <okothdenzel65@gmail.com>
2025-11-30 07:59:47 +0300

    implements a custom created at field

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import Book
+from django.utils import timezone
 
 class BookSerializer(serializers.ModelSerializer):
-    class Meta: 
-        model = Book
-        fields = '__all__'
+    created_at = serializers.SerializerMethodField()
+    
+    class Meta:
+        model=Book
+        fields=['id', 'title', 'author', 'published_date', 'created_at']
         
+    def get_created_at(self, obj):
+        return timezone.now().strftime('%Y-%m-%d %H:%M:%S')
