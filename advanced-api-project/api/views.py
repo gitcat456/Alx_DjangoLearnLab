26a8b956be81e32209855a4dbe524e23624658f6
gitcat456 <okothdenzel65@gmail.com>
2025-11-30 11:21:18 +0300

    checker requirements

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,12 +1,22 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
-
+from django_filters.rest_framework import DjangoFilterBackend
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    
+     # Step 1: Filtering
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    
+    # Step 2: Search
+    search_fields = ['title', 'author__name']
+    
+    # Step 3: Ordering  
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']  # Default order
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
