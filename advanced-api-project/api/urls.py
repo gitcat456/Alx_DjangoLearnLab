16813b7631b5f0346708b45078efba1ff35c5177
gitcat456 <okothdenzel65@gmail.com>
2025-11-30 10:54:13 +0300

    checker requirements

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -9,8 +9,8 @@ from .views import(
 
 urlpatterns = [
     path('books/', BookListView.as_view()),
-    path('books/', BookCreateView.as_view()),
+    path('books/create', BookCreateView.as_view()),
     path('books/<int:pk>/', BookDetailView.as_view()),
-    path('books/<int:pk>/update/', BookUpdateView.as_view()),
-    path('books/<int:pk>/delete/', BookListView.as_view()),
+    path('books/update/<int:pk>/', BookUpdateView.as_view()),
+    path('books//delete/<int:pk>', BookDeleteView.as_view()),
 ]
