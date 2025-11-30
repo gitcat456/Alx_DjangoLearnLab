61eec01bdb49f3ca276ad8c396525d8dac35a7b2
gitcat456 <okothdenzel65@gmail.com>
2025-11-30 08:00:47 +0300

    use generics module to create api endpoints

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,22 +1,124 @@
-from django.shortcuts import render
-from rest_framework import generics, viewsets
-from .serializers import BookSerializer
 from .models import Book
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny, IsAuthenticatedOrReadOnly
+from .serializers import BookSerializer 
+from rest_framework import generics
 
-class BookList(generics.ListAPIView):
+class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [AllowAny]
 
-class BookViewSet(viewsets.ModelViewSet):
-     queryset = Book.objects.all()
-     serializer_class = BookSerializer
-    # permission_classes = [IsAuthenticatedOrReadOnly] 
+class BookUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDelete(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.shortcuts import render
+# from rest_framework import generics, viewsets
+# from .serializers import BookSerializer
+# from .models import Book
+# #from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny, IsAuthenticatedOrReadOnly
+
+# class BookList(generics.ListAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+#     #permission_classes = [AllowAny]
+
+# class BookViewSet(viewsets.ModelViewSet):
+#      queryset = Book.objects.all()
+#      serializer_class = BookSerializer
+#     # permission_classes = [IsAuthenticatedOrReadOnly] 
     
-     def get_permissions(self):
-        if self.action == 'list' or self.action == 'retrieve':
-            permission_classes = [AllowAny]
-        else:
-            permission_classes = [IsAuthenticated]
-        return [permission() for permission in permission_classes]
+#     # def get_permissions(self):
+#         #if self.action == 'list' or self.action == 'retrieve':
+#            # permission_classes = [AllowAny]
+#        # else:
+#             #permission_classes = [IsAuthenticated]
+#        # return [permission() for permission in permission_classes]
