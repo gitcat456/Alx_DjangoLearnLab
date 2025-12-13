95624699ffe1e0c4c3a535b9881eae7f604c9b9f
gitcat456 <okothdenzel65@gmail.com>
2025-12-13 10:47:56 +0300

    creates a custom user model

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    bio = models.TextField(max_length=500, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following',
+        blank=True
+    )
+    
+    def __str__(self):
+        return self.username
+    
+    def followers_count(self):
+        return self.followers.count()
+    
+    def following_count(self):
+        return self.following.count()
