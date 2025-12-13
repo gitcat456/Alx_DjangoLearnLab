567985cb6fc8010da39044233770bde630c6956f
gitcat456 <okothdenzel65@gmail.com>
2025-12-13 11:42:05 +0300

    enhances the view logic

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets, permissions, filters
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
@@ -33,3 +36,21 @@ class CommentViewSet(viewsets.ModelViewSet):
     
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_feed(request):
+    """Get posts from users the current user follows"""
+    # Get IDs of users being followed
+    following_ids = request.user.following.values_list('id', flat=True)
+    
+    # Get posts from followed users, ordered by newest first
+    posts = Post.objects.filter(author_id__in=following_ids).order_by('-created_at')
+    
+    # Paginate
+    paginator = StandardPagination()
+    result_page = paginator.paginate_queryset(posts, request)
+    
+    # Serialize
+    serializer = PostSerializer(result_page, many=True)
+    return paginator.get_paginated_response(serializer.data)
