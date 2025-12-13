aaecc967f81916315c07e9ae56418f7dd6f94f58
gitcat456 <okothdenzel65@gmail.com>
2025-12-13 11:59:41 +0300

    adds like model

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 class Post(models.Model):
     author = models.ForeignKey(
@@ -44,3 +46,15 @@ class Comment(models.Model):
     
     def __str__(self):
         return f"Comment by {self.author.username} on {self.post.title}"
+    
+    
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ['user', 'post']  # Prevent duplicate likes
+    
+    def __str__(self):
+        return f"{self.user.username} likes {self.post.title}"
