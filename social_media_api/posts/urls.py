f3bbcc8c07b4612969f059118791463dc58d0b90
gitcat456 <okothdenzel65@gmail.com>
2025-12-13 12:07:46 +0300

    enhance to meet checker requirements

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet, user_feed
+from .views import PostViewSet, CommentViewSet, user_feed, LikePostView, UnlikePostView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -11,6 +11,6 @@ urlpatterns = [
      # Add feed endpoint
     path('feed/', user_feed, name='user-feed'),
     
-    path('posts/<int:pk>/like/', PostViewSet.as_view({'post': 'like'}), name='post-like'),
-    path('posts/<int:pk>/unlike/', PostViewSet.as_view({'post': 'unlike'}), name='post-unlike'),
+    path('posts/<int:pk>/like/', LikePostView.as_view(), name='post-like'),
+    path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='post-unlike'),
 ]
