53d002fdad36ba6a740ad029abb3b7463bbbb77e
gitcat456 <okothdenzel65@gmail.com>
2025-12-13 12:02:39 +0300

    notification functionalities

--- social_media_api/notifications/views.py
+++ social_media_api/notifications/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .models import Notification
 
-# Create your views here.
+class NotificationListView(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        return Notification.objects.filter(recipient=self.request.user)
+    
+    def list(self, request):
+        notifications = self.get_queryset()
+        data = []
+        for notification in notifications:
+            data.append({
+                'id': notification.id,
+                'actor': notification.actor.username,
+                'verb': notification.verb,
+                'read': notification.read,
+                'timestamp': notification.timestamp
+            })
+        return Response(data)
